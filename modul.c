55318bad29ad71bf877b97e3614a0b0c76f7354d
Oleksij Rempel <external.Oleksij.Rempel@de.bosch.com>
2015-06-04 15:39:26 +0200

    modul.c
    Signed-off-by: Oleksij Rempel <external.Oleksij.Rempel@de.bosch.com>

--- modul.c
+++ modul.c
@@ -1,17 +1,33 @@
+/*
+ * Copyright (c) 2015 Oleksij Rempel <linux@rempel-privat.de>.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <linux/init.h>
 #include <linux/module.h>
-MODULE_LICENSE("GPL");
 
 static int hrtimer_test_init(void)
 {
-	pr_info("Hello, world\n");
+	pr_info("%s\n",__func__);
 	return 0;
 }
 
 static void hrtimer_test_exit(void)
 {
-	pr_info("Goodbye, cruel world\n");
+	pr_info("%s\n",__func__);
 }
 
 module_init(hrtimer_test_init);
 module_exit(hrtimer_test_exit);
+MODULE_LICENSE("GPL");
